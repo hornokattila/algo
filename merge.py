2893513682800f9b24b57594466bf58e63f0c3f7
hornokattila <hornokattila@outlook.com>
2024-09-10 18:24:48 +0200

    Improved loop recognition.

--- merge.py
+++ merge.py
@@ -1,9 +1,9 @@
 class Merge:
     def __init__(self, array):
         self.array = array
+        print("I: {}".format(self.array))
         # Turns true for processed indexes.
         self.debug = [False for _ in range(len(array))]
-        print("I: {}".format(self.array))
 
     def _calculate_shift(self, index):
         if index < round((len(self.array) + .5) / 2):
@@ -15,17 +15,21 @@ class Merge:
     def _calculate_new_index(self, index):
         return index + self._calculate_shift(index)
 
-    def _calculate(self, index, value, ttl):
+    def _calculate(self, index, value):
         _forward_index = self._calculate_new_index(index)
         _forward_value = self.array[_forward_index]
-        if ttl > 0:
-            self.array[_forward_index] = value
-            self.debug[_forward_index] = True
-            self._calculate(_forward_index, _forward_value, ttl - 1)
+        print("index: {}, value: {} -> forward index: {} | (forward value: {})"
+              .format(index, value, _forward_index, _forward_value))
+        self.array[_forward_index] = value
+        self.debug[_forward_index] = True
+        if _forward_value:
+            self._calculate(_forward_index, _forward_value)
 
     def calculate(self):
         if len(merge.array) > 2:
-            self._calculate(1, self.array[1], len(merge.array) - 1)
+            value = self.array[1]
+            self.array[1] = None
+            self._calculate(1, value)
         print("O: {}".format(self.array))
 
 
@@ -35,12 +39,13 @@ def _prepare_test(st_half_size, nd_half_size):
 
 
 if __name__ == '__main__':
-    for i in range(2, 10):
-        for j in range(0,2):
-            merge = Merge(_prepare_test(i, i-j))
+    for i in range(1, 5):
+        for j in range(0, 2):
+            merge = Merge(_prepare_test(i, i - j))
             merge.calculate()
+            # In case of even length input it's okay for the last element not to be processed.
             for k in range(1, len(merge.debug)):
-                # In case of even length input it's okay for the last element not to be processed.
                 if j != 0 and k != len(merge.debug) -1 and not merge.debug[k]:
-                    print("DEBUG: Following index was not processed: {}".format(k))
+                    print("ERROR: Something was not processed at index {}".format(k))
+                    exit(1)
             print("---")
