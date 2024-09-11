ccb0f8314195805dc6b391c26f464e8b5cd4494c
hornokattila <hornokattila@outlook.com>
2024-09-11 17:05:52 +0200

    Renamings.

--- merge.py
+++ merge.py
@@ -15,7 +15,7 @@ class Merge:
     def _calculate_new_index(self, index):
         return index + self._calculate_shift(index)
 
-    def _calculate(self, index, value):
+    def _sort(self, index, value):
         _forward_index = self._calculate_new_index(index)
         _forward_value = self.array[_forward_index]
         # print("index: {}, value: {} -> forward index: {} | (forward value: {})"
@@ -23,26 +23,26 @@ class Merge:
         self.array[_forward_index] = value
         self.debug[_forward_index] = True
         if _forward_value:
-            self._calculate(_forward_index, _forward_value)
+            self._sort(_forward_index, _forward_value)
 
-    def calculate(self):
+    def run(self):
         if len(merge.array) > 2:
             value = self.array[1]
             self.array[1] = None
-            self._calculate(1, value)
+            self._sort(1, value)
         print("O: {}".format(self.array))
 
 
 def _prepare_test(limit):
     # Test method that returns input data using even and odd numbers for the sake of readability.
-    array = [i +1 for i in range(limit)]
+    array = [i + 1 for i in range(limit)]
     return [*array[::2], *array[1::2]]
 
 
 if __name__ == '__main__':
-    for i in range(0, 10):
+    for i in range(0, 12):
         merge = Merge(_prepare_test(i))
-        merge.calculate()
+        merge.run()
         # In case of even length input it's okay for the last element not to be processed.
         for k in range(1, len(merge.debug)):
             if i % 2 == 0 and k == len(merge.debug) - 1:
